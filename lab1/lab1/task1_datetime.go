2da264b18b4fa7f7a6b7edfc25b0eb02c6d69def
PEpsi-cpu <yabogdan29@gmail.com>
2026-09-24 09:52:43 +0300

    Update task1_datetime.go

--- lab1/lab1/task1_datetime.go
+++ lab1/lab1/task1_datetime.go
@@ -1,1 +1,13 @@
+package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Println("Текущая дата и время:", now.Format("02.01.2006 15:04:05"))
+	fmt.Println("Дата:", now.Format("02 January 2006"))
+	fmt.Println("Время:", now.Format("15:04:05"))
+}
